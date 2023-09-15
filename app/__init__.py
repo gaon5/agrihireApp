9bc066966b12d7e426bca7bb7b265c43c2f4b160
LeoZhao <Leo.Zhao@lincolnuni.ac.nz>
2023-09-15 22:51:51 +1200

    Create template

---
+++ app/__init__.py
@@ -0,0 +1,47 @@
+from flask import Flask, url_for, request, redirect, render_template, session
+from datetime import date, datetime, timedelta
+import mysql.connector
+from app import config
+import math
+import bcrypt
+import re
+
+app = Flask(__name__)
+app.config.from_object(config)
+app.config['PERMANENT_SESSION_LIFETIME'] = 86400
+app.secret_key = 'aHn6Zb7MstRxC8vEoF2zG3B9wQjKl5YD'
+
+db_conn = None
+connection = None
+
+
+def get_cursor():
+    global db_conn
+    global connection
+    connection = mysql.connector.connect(user=config.dbuser,
+                                         password=config.dbpass,
+                                         host=config.dbhost,
+                                         database=config.dbname,
+                                         autocommit=True)
+    db_conn = connection.cursor()
+    return db_conn
+
+
+def check_permissions():
+    is_member = session['is_member']
+    is_instructor = session['is_instructor']
+    is_admin = session['is_admin']
+    is_root = session['is_root']
+    if is_root == 1:
+        return 4
+    elif is_admin == 1:
+        return 3
+    elif is_instructor == 1:
+        return 2
+    elif is_member == 1:
+        return 1
+    else:
+        return 0
+
+
+from app import admin, member, root, guest
---
+++ app/guest.py
@@ -0,0 +1,47 @@
+from flask import Flask, url_for, request, redirect, render_template, session
+from datetime import date, datetime, timedelta
+import math
+import bcrypt
+import re
+from app import app, check_permissions, get_cursor
+
+
+@app.route('/')
+def index():
+    return render_template('guest/index.html')
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    return render_template('guest/login.html')
+
+
+@app.route('/logout')
+def logout():
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('guest/register.html')
+
+
+@app.route('/change_password', methods=['GET', 'POST'])
+def change_password():
+    return render_template('guest/change_password.html')
+
+
+@app.route('/change_information', methods=['GET', 'POST'])
+def change_information():
+    return render_template('guest/change_information.html')
+
+
+# @app.errorhandler(Exception)
+# def handle_error(error):
+#     """
+#     Receive all unexpected errors
+#     :param error:
+#     :return: error.html
+#     """
+#     print(error)
+#     return render_template('guest/error.html', permissions=check_permissions())
