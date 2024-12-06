3fe23508d00511220979dcdca5c9f43dd19d8b18
Summer <na.gao@lincolnuni.ac.nz>
2024-12-07 02:21:46 +1300

    .env added

--- app/config.py
+++ app/config.py
@@ -1,3 +1,29 @@
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path=".env")
+
+
+
+dbpass = os.getenv("DB_PASSWORD")
+dbhost = "nagao5.mysql.pythonanywhere-services.com"
+dbport = "3306"
+dbname = "nagao5$agrihire_db"
+dbuser = "nagao5"
+
+# dbpass = "12345678"
+# dbhost = "127.0.0.1"
+# dbport = "3306"
+# dbname = "agrihire"
+# dbuser = "root"
+
+# dbuser = "root" #PUT YOUR MySQL username here - usually root
+# dbpass = "12345678" #PUT YOUR PASSWORD HERE
+# dbhost = "localhost" 
+# dbport = "3306"
+# dbname = "presentation hire"
+
 # dbpass = "88LcNrALs5NpRjjF"
 # dbhost = "leoselfcloud.top"
 # dbport = "3306"
@@ -16,17 +42,7 @@
 # dbname = "hire"      
 # dbuser = "root"
 
-dbpass = "12345678"
-dbhost = "127.0.0.1"
-dbport = "3306"
-dbname = "agrihire"
-dbuser = "root"
 
-# dbuser = "root" #PUT YOUR MySQL username here - usually root
-# dbpass = "12345678" #PUT YOUR PASSWORD HERE
-# dbhost = "localhost" 
-# dbport = "3306"
-# dbname = "presentation hire"
 
 # dbuser = "root" 
 # dbpass = "Rooting23"
